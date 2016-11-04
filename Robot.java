ef751a85821cabf64504b5328f02e55d0b4229bb
sambarth <samuel-barth@uiowa.edu>
2016-11-04 11:16:30 -0500

    A rough draft of a robot class, including getters and setters but nothing else

---
+++ Robot.java
@@ -0,0 +1,33 @@
+/*
+*
+*/
+
+Class Robot {
+
+  private int x = 0;
+  private int y = 0;
+  private int distanceTraversed = 0;
+  private boolean isIdle = true;
+
+  public Robot(int x, int y){
+    this.x = x;
+    this.y = y;
+  }
+  //getters and setters
+  public getX(){
+    return x;
+  }
+  public getY(){
+    return y;
+  }
+  public getDistanceTraversed(){
+    return distanceTraversed;
+  }
+  public isIdle(){
+    return isIdle;
+  }
+  // control methods:
+  // if idle, Robots will need to request new commands (from ?) (if applicable)
+  // at each new tick (from Master?).
+  // if not idle, Robots will need to request a path to travel (from Floor?)
+}
