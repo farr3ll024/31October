ac8d7910ce3bda9ccd68f5f0a7082e78f2def19d
qw123qw0324 <qw123qw0324@gmail.com>
2016-11-06 22:38:19 -0600

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -1,16 +1,50 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 
-public class InventoryList{
+public class Inventory {
+	boolean isExist;
+	ArrayList<String> itemList = new ArrayList<String>();
 	
-	private static HashMap<String,ArrayList> inventoryList = new HashMap<String,ArrayList>();
-	private static ArrayList<String> itemDetails = new ArrayList<String>();
+    public Inventory(ArrayList itemList){
+        this.itemList = itemList;
+    }
+    
+    public void removeItem(String itemNum){
+    	if (isExist){
+    		/*remove item from itemList*/
+    	}
+    	else{
+    		
+    	}
+    		
+    }
+    
+    public void addItem(String itemNum){
+    	itemList.add(itemNum);
+    }
+    
+    public boolean checkExist(String itemNum){
+    	/* add code here to check existence, 
+    	 * if exist isExist is true
+    	 * else false*/
+    	return isExist;
+    }
+    
+}
 
-	
+
+class Test {
+	public static void main(String[] args) {
+		
+		ArrayList<String> listA = new ArrayList<String>();
+		/*add code here to read file and insert the item in to listA*/
+	    
+		Inventory a = new Inventory(listA);
+	    a.addItem("1111");
+	    a.removeItem("1111");
+	}
 
 }
 
-//import java.util.HashMap;
-//private static interface HashMap<String,Object> orderInfo = new HashMap<>();
+
 
 
