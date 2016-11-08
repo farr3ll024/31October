d505aab0143f2f3d5e1dd7ecf32c0be5a4e9f853
farr3ll <farr3ll@black-box>
2016-11-08 13:12:40 -0600

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/farr3ll024/31October.git
    
    Signed-off-by: farr3ll <farr3ll@black-box>

--- Master.java
+++ Master.java
@@ -8,94 +8,54 @@ package oct31;
 
 
 public class Master {
-	Boolean isCompleted;
-	Boolean isRunning;
-	Task current_task;
+	Boolean isRunning;//aka status
+	int interations;// number of iterations("ticks") in this simulation
+	int current_iteration;
 	// constructor for master simulation
 	public Master() {
-		this.isCompleted = false;
 		this.isRunning = null;
-		this.current_task = null;
+		this.iterations = 100;
+		this.current_iteration = 0;
 	}
 	// simulation is started
-	public void triggered() {
+	public void startSim() {
 		this.isRunning = true;
 	}
-	// fire method executes the current task
-	public void fire() {
-		// move robot/mark task as completed/etc.
-	}
-	// method for mark completed
-	public void markCompleted() {
-		this.isCompleted = true;
-		this.isRunning = false;
-	}
-	// method for event status
-	public boolean isCompleted() {
-		return false;
-	}
-	// create list of events
-	public void generate_tasks() { // param will need to be the list of orders
-		// events will need to be based off orders
-	}
-	// pause sim. in order to add visual
-	public void buffer() {
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	// simulation satus
+	public Boolean getStatus() {
+		if(this.current_iteration==100) {
+			this.isRunning = false;
 		}
+		return this.isRunning;
 	}
-	// document simulation
-	public void doc() {
-		while (this.isRunning) {
-			System.out.println("In progress");
-		}
-		System.out.println("Completed");
+	//simulation end
+	public void endSim() {this.isRunning = false;}
+	//get iteration
+	public int getIteration() {return this.current_iteration}
+	//clock interface
+	public interface clock{
+		void tick(int iteration);
 	}
-	// method for setting the current task(assigns current task to sim object)
-	public void setTask() {
-
+	//documentation interface
+	public interface document{
+		String doc(int iteration);// use this if you want the master to record the info to one file
+		void doc(String fileName);// this would probably be easier because people could create their own documentation
 	}
-
-	// class for Events(contains hashmap of events)
-	public class Task {
-		public Task() {
-			// where to go
-			// what to get
-			// etc.
-			// very dependent on order class
-		}
-	}
-
-	// interface for tasks(should be implemented by all classes)
-	public interface simulate {
-		// provides useful methods to work with current task
-		void setTask(); // this will get the next task that should be
-							// completed
-		void fire(); // this will execute the task - ex) event.fire()
-		void markCompleted();// marks the current task as finished - ex)
-								// event.markCompleted()
-		void isCompleted(); // check if the current task is completed. if it
-								// is get the next task - ex)
-								// event.isCompleted()
-		void buffer(); // pause simulation in order to add visual/realism
-	}
-
 	// main simulation
 	public static void main(String args[]) {
+		//sim objects(initialization)
+		//Robot bot = new Robot(); 
+		//etc.... 
+		
 		Master sim = new Master();// create simulation object
-		sim.generate_tasks(); // need order params
-
-		while (sim.isCompleted() == false) {
-			sim.triggered();
-			sim.setTask();
-			sim.buffer();
-			sim.fire();
-			sim.buffer();
-			sim.doc();
-			sim.buffer();
+		sim.startSim();// start the simulation
+		while (sim.getStatus() == true) {
+			//bot.tick(sim.current_iteration);
+			//etc...
+			//bot.doc(fileName);
+			//etc...
+			System.out.println(sim.current_iteration);
+			sim.current_iteration+=1;
 		}
 	}
 }
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # 31October
 CS2820 group project
+
+I changed the format of the master in order to make it so the objects can work more independently rather than depending on a specific "event/simulate" structure. 
+	1) I've provided a documentation interface plus a clock interface(with a tick method)
+	2) the master simulation will create an instance of all of the objects(i.e. new Robot, new Belt...etc)
+	3) on each iteration(will be a default 100 iterations to start) the master simulation will invoke the tick method with a reference to each object(which will allow you guys to operate with more of your own methods/respond accordingly instead of me trying to make specific methods for all of your objects)
+	4) the documentation interface will provide a method to receive a file name(which should be used to record object specific data) Note: the file name will be pushed to the objects via the master class 
+	5) Note: the simulation will include a buffer in each iteration in order to allow time for a visual aspect
+	
+	** feel free to recommend something different or point out something I've missed. 
+	
+	
