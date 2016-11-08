2d127b25eb55bf93322bd4f3b6c1e17b8ab9322e
sambarth <samuel-barth@uiowa.edu>
2016-11-07 23:59:15 -0600

    added an outline for the deployIdleRobot method to RobotMaster

--- RobotMaster.java
+++ RobotMaster.java
@@ -4,17 +4,31 @@ import java.util.ArrayList
 
 Class RobotMaster implements MockTimer{
 
-    arrayList<Robot> robots;
+    ArrayList<Robot> robots;
 
     //intialize RobotMaster with some starting number of Robots
     RobotMaster(int numRobots){
-        this.robots = new arrayList<Robot>(int);
+        this.robots = new ArrayList<Robot>(int);
         //the following for loop will individually initialize each robot at a charge location
         for (Robot r : this.robots){
             r= new Robot;
+            this.robots.add(r);
         }
     }
-
+    //the instuctions for the Robot will be passed to deployIdle
+    public void deployIdle(String temp){
+        boolean robotAvailable = false;
+        Robot availableRobot;
+        for (Robot r : this.robots){
+            if (r.isIdle()){
+                robotAvailable = true;
+                availableRobot = r;
+                break;
+            }
+        //here we'd like to give Robot "availableRobot" specific instructions
+        //about how to complete its mission
+        }
+    }
 
 
 }
