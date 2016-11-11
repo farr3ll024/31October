8388c6eee34ea76c24fc24efcba96ebe4c71188a
tfinley521 <trevor-finley@uiowa.edu>
2016-11-10 22:49:04 -0600

    Slightly more work has been made, Point works now
    Still heavily unfinished.

--- Floor.java
+++ Floor.java
@@ -1,49 +1,54 @@
+import java.awt.Point;
+
 public class Floor
 {
     Point charger = new Point(0,0);
-    Point rdock = new Point(0, 15);
+    Point rdock = new Point(0,15);
     Point[] shelf;
-    int shelflimit = 0;
     
     //for loop that constructs shelves and their respective coordinates
-    for(int s=1, int x=3; x<=13; x++)
+    
+    public Point[] constructShelf()
     {
+    	for(int s=1, x=3; x<=13; x++)
+    	{
         //catches highway gaps
         if(x ==5 || x== 8 || x== 11)
             continue;
 
-        for(int y=2; y<=12; y++)
-        {
-            //catches highway gaps
-            if(y == 7)
-                continue;
+        	for(int y=2; y<=12; y++)
+        	{
+        		//catches highway gaps
+        		if(y == 7)
+        			continue;
                 
-            else
-            {
-                shelf[s] = (x,y);
-                s++;
-                shelflimit = s;
+        		else
+        		{
+        			shelf[s] = new Point(x,y);
+        			s++;
                 
-            }
+        		}
         }
         
+    	}
+    	
+    	return shelf;
     }
-    
     public int getShelfLimit()
     {
-        return shelflimit;
+        return shelf.length;
     }
-    public Point getShelf(int n)
-    {
-        if (n > shelflimit || n<1)
-        {
-            System.out.println("ERROR: must specify a valid shelf number between 1 and " + shelflimit);
-            System.out.println("Please change the code to match.");
-            return Point();
-        }
-        
-        return shelf[n];
-    }
+//    public Point getShelf(int n)
+//    {
+//        if (n > shelf.length || n<1)
+//        {
+//            System.out.println("ERROR: must specify a valid shelf number between 1 and " + shelflimit);
+//            System.out.println("Please change the code to match.");
+//            return Point();
+//        }
+//        
+//        return shelf[n];
+//    }
 
 //     public static void constructMap()
 //     {
