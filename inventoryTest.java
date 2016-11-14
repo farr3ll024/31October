3d0791291d449121168f35148657b0171ae53539
qw123qw0324 <qw123qw0324@gmail.com>
2016-11-14 02:05:14 -0600

    Update inventoryTest.java

--- inventoryTest.java
+++ inventoryTest.java
@@ -1,3 +1,9 @@
+/**
+ *
+ * @author Ming Cheng
+ *
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,17 +16,23 @@ import java.util.List;
 import java.util.Map;
 
 public class inventoryTest {
-	boolean isExist;
+	boolean isExist; //variable to chech whether item exist or not
 	//boolean isInList;
-	int amount;
-	List<Map<String, Object>> inventory = new ArrayList<Map<String, Object>>();
+	int amount; // the amount of item
+	List<Map<String, Object>> inventory = new ArrayList<Map<String, Object>>(); // ArrayList to save the data if the list
 	
+    // Constructor
     public inventoryTest(List inventory){
-        this.inventory = inventory;
+        this.inventory = inventory; 
     }
-    
+   
+	
+    // Read file and add data of the file to ArrayList
+    /**
+     * @param nothing
+     */
     public void data(){
-    	
+ 
     	BufferedReader br = null;
 		try
 		{
@@ -30,7 +42,8 @@ public class inventoryTest {
 			e.printStackTrace();
 			return;
 		}
-
+               
+	       
 		String[] columnName =
 		{ "Id", "Name", "Amount"}; 
 		int i, index;
@@ -42,7 +55,7 @@ public class inventoryTest {
 			{
 				index = 0;
 				String[] se = line.split(" ");
-				Map<String, Object> item = new HashMap<String, Object>();
+				Map<String, Object> item = new HashMap<String, Object>(); // create item
 				for (i = 0; i < se.length; i++)
 				{
 					if ("".equals(se[i]))
@@ -60,18 +73,20 @@ public class inventoryTest {
 				int amount = Integer.parseInt((String) item
 							.get(columnName[2]));
 				
+				
 				if (amount > 0){
 					item.put("Existence", "Y");
 				}
 				else{
 					item.put("Existence", "N");
 				}
-
-				inventory.add(item);
+         
+				inventory.add(item); // add item to ArrayList
 			}
 			br.close();
             
 			outPutFile();
+			
 		} catch (IOException e)
 		{
 			e.printStackTrace();
@@ -79,7 +94,14 @@ public class inventoryTest {
     	
     }
     
+	
+    //remove item from list
+    /**
+     * @param itemName, the name of the item that we want to remove from the list
+     */
     public void removeItem(String itemName){
+
+	//check whether the item is exist or not
     	if (checkExist(itemName) == true){
     		int i;
     		//System.out.println(checkExist(itemName));
@@ -88,10 +110,12 @@ public class inventoryTest {
         		Map<String, Object> removedItem = new HashMap<String, Object>();
         		removedItem = inventory.get(i);
         		
+			//find the item that we want to remove in the List
     			if (itemName.equals(removedItem.get("Name").toString())){
     				int a = Integer.parseInt((String) removedItem.get("Amount"));
     				removedItem.put("Amount",a-1);
     				
+				// if the number of item is 0, then Existence is N
     				if (a-1 < 1){
     					removedItem.put("Existence","N");
     				}
@@ -99,22 +123,34 @@ public class inventoryTest {
     		}
     	}
     	
+	//item not exist
     	else{
     		System.out.println("Item not exist");
     		checkExist(itemName);
     	}
     	
+	
     	outPutFile();
     		
     }
     
+	
+    //add item to the list
+    //Variable InList is used to check whether the item is already in list or not
+    //if already in list, when we add item, we just need to update the amount of the item
+    //if not in list, we need to create a new line in the list
+    /**
+     * @param itemName, the name of the item that we want to add into the list
+     */
     public void addItem(String itemName){
+	
     	boolean InList = false;
     	int i;
     	for (i = 0; i < inventory.size(); i++)
 		{
     		Map<String, Object> newItem = new HashMap<String, Object>();
     		newItem = inventory.get(i);
+		        //if item is already in the list
 			if (itemName.equals(newItem.get("Name").toString())){
 				int a = Integer.parseInt((String) newItem
 						.get("Amount"));
@@ -124,6 +160,7 @@ public class inventoryTest {
 			}
 		}
     	
+	//if item not in list yet
     	if (InList == false){
     		Map<String, Object> newItem = new HashMap<String, Object>();
     		newItem.put("Id", i+1);
@@ -137,7 +174,13 @@ public class inventoryTest {
          
     } 
     
+    //output the file
+    /**
+     * @param nothing
+     */
     public void outPutFile(){
+	
+	    
     	try
 		{
     	   PrintWriter pw = new PrintWriter(new File("/Users/macbook_user/Desktop/OOP Project/List2.txt"));
@@ -161,8 +204,16 @@ public class inventoryTest {
 		}
     }
     
+	
+    //check whether the item exist or not
+    //if the amount of item is 0, then item is not exist
+    //if item is not in the list, then item is not exist
+    /**
+     * @param itemName, the name of the item that we want to check whether exist or not
+     * @return
+     */
     public boolean checkExist(String itemName){
-    	
+    	 
     	for (int i = 0; i < inventory.size(); i++)
 		{
     		Map<String, Object> newItem = new HashMap<String, Object>();
@@ -186,9 +237,11 @@ public class inventoryTest {
     	return isExist;
     	
     }
-    
+   
+	
+    //used to test
     public static void main(String[] args) {
-		
+
 		List<Map<String, Object>> listA = new ArrayList<Map<String, Object>>();
 		/*add code here to read file and insert the item in to listA*/
 	    
