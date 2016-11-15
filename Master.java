7245b625a2b5eb3b496299579aeb6e67bead06c8
Blaise <blaise-reints@uiowa.edu>
2016-11-15 07:49:34 -0600

    Add files via upload

--- Master.java
+++ Master.java
@@ -13,34 +13,63 @@
  * a loop that sends an iteration to all of the class instances
  */
 package oct31;
+
+/**
+ *
+ * @author farr3ll
+ */
 public class Master {
 	Boolean isRunning;//aka status
 	int iterations;// number of iterations("ticks") in this simulation
 	int current_iteration;
 	// constructor for master simulation
-	public Master() {
+
+    /**
+     * constructor for master class
+     */
+    public Master() {
 		this.isRunning = null;
 		this.iterations = 100;
 		this.current_iteration = 0;
 	}
-	// simulation is started
-	public void startSim() {
+
+    /**
+     * method for starting simulation
+     */
+    public void startSim() {
 		this.isRunning = true;
 	}
-	// simulation satus
-	public Boolean getStatus() {
-		if(this.current_iteration==100) {
+
+    /**
+     * method for returning the status of the simulation(running/not running)
+     *
+     * @return Boolean
+     */
+    public Boolean getStatus() {
+        if (this.current_iteration == 100) {
 			this.isRunning = false;
 		}
 		return this.isRunning;
 	}
-	//simulation end
-	public void endSim() {this.isRunning = false;}
+
+    /**
+     * method ends the simulation
+     */
+    public void endSim() {
+        this.isRunning = false;
+    }
 	//get iteration
-	public int getIteration() {
+
+    /**
+     * method returns the current iteration number
+     *
+     * @return int
+     */
+    public int getIteration() {
         return this.current_iteration;
     }
-	//clock interface
+
+    /*clock interface
 	public interface clock{
 		void tick(int iteration);
 	}
@@ -48,9 +77,13 @@ public class Master {
 	public interface document{
 		String doc(int iteration);// use this if you want the master to record the info to one file
 		void doc(String fileName);// this would probably be easier because people could create their own documentation
-	}
-	// main simulation
-	public static void main(String args[]) {
+	}*/
+    /**
+     * main method
+     *
+     * @param args
+     */
+    public static void main(String args[]) {
 		//sim objects(initialization)
 		//Robot bot = new Robot(); 
 		//etc.... 
