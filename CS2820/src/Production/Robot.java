02da4877ba21f62769196ad312ba8a100048854c
sambarth <samuel-barth@uiowa.edu>
2016-11-17 11:49:05 -0600

    Testing that I have the ability to make commits to the new repository/directory tree

--- CS2820/src/Production/Robot.java
+++ CS2820/src/Production/Robot.java
@@ -1,109 +1,143 @@
-package Production;
-
 /**
- *
- * @author: Sam Barth
- *
- * date last modified: 11/14/2016
- *
- * The Robot class defines the individual robot. An new instance of Robot will
- * have a starting x coordinate "0" and some y coordinate less than the number
- * of robots in the warehouse. Robots will always be in one of two states: idle
- * (ready to be assigned a mission) or not idle (currently performing a mission)
- */
-public class Robot {
+*
+* @author: Sam Barth
+*
+* date last modified: 11/14/2016
+* 
+* The Robot class defines the individual robot.  An new instance of Robot
+* will have a starting x coordinate "0" and some y coordinate less than
+* the number of robots in the warehouse.  Robots will always be in one of two
+* states: idle (ready to be assigned a mission) or not idle (currently
+* performing a mission)
+*/
 
-    private final int xCharge = 0;
-    private int yCharge = 0;
-    private int x = 0;
-    private int y = 0;
-    private int distanceTraversed = 0;
-    private boolean isIdle;
-    private boolean shelfCoupled;
-    private int coupledShelfID;
-    private boolean onOrderMission;
-    private boolean onStockMission;
+package oct31;
 
-    public Robot(int x, int y) {
-        this.x = x;
-        this.y = y;
-        this.isIdle = true;
-        this.distanceTraversed = 0;
-        this.yCharge = y;
-        this.shelfCoupled = false;
-        this.onOrderMission = false;
-        this.onStockMission = false;
-    }
-    //I'll certainly end up changing how "direction" is passed to the robot to improve efficiency
+import java.awt.Point;
 
-    private void move(int x_destination, int y_destination) {
-        if (this.x != x_destination) {
-            if (this.x < x_destination) {
-                this.x += 1;
-            } else {
-                this.x -= 1;
-            }
-        } else if (this.y < y_destination) {
-            this.y += 1;
-        } else {
-            this.y -= 1;
-        }
-        this.distanceTraversed += 1;
-    }
+public class Robot{
 
-    public void returnToCharger() {
-        this.move(this.xCharge, this.yCharge);
-    }
+  private final int xCharge = 0;
+  private int yCharge = 0;
+  private int x = 0;
+  private int y = 0;
+  private int distanceTraversed = 0;
+  private boolean isIdle;
+  private boolean shelfCoupled;
+  private int coupledShelfID;
+  private boolean onOrderMission;
+  private boolean onStockMission;
 
-    public void fetchItem(int shelfLocation) {
-        /*
+  /**
+   * 
+   * @param x The starting x location for the Robot
+   * @param y The starting y location for the Robot
+   */
+  public Robot(int x, int y){
+    this.x = x;
+    this.y = y;
+    this.isIdle = true;
+    this.distanceTraversed = 0;
+    this.yCharge = y;
+    this.shelfCoupled = false;
+    this.onOrderMission = false;
+    this.onStockMission = false;
+  }
+  //I'll certainly end up changing how "direction" is passed to the robot to improve efficiency
+  private void move(int x_destination, int y_destination){
+      if (this.x != x_destination){	  
+    	  if (this.x < x_destination){
+    		  this.x += 1;
+    	  }
+    	  else {this.x -= 1;}
+      }
+      else {
+    	  if (this.y < y_destination){
+    		  this.y += 1;
+    	  }
+    	  else {this.y -= 1;}
+      }
+      this.distanceTraversed += 1;
+  }  
+  public void returnToCharger(){
+	  this.move(this.xCharge, this.yCharge);
+  }
+  /**
+   * 
+   * @param shelfLocation The unique id corresponding to a shelf on the floor
+   */
+  public void fetchItem(int shelfLocation){
+	  /*
 	   * if shelfCoupled, this.move(x.picker, y.picker)
 	   * else this.move(x.shelfLocaiton, y.shelfLocation)
-         */
-    }
-
-    public void stockItem(int shelfLocation) {
-        /*
+	   */
+  }
+  /**
+   * 
+   * @param shelfLocation The unique id corresponding to the shelf on the floor
+   */
+  public void stockItem(int shelfLocation){
+	  /*
 	   * if not shelfCoupled, this.move(x.shelfLocation, y.shelfLocation)
 	   * else this.move(x.receivingDock, y.receivingDock)
-         */
-    }
-    //getters and setters
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getDistanceTraversed() {
-        return distanceTraversed;
-    }
-
-    public boolean isIdle() {
-        return isIdle;
-    }
-
-    public boolean shelfCoupled() {
-        return shelfCoupled;
-    }
-
-    public void coupleShelf() {
-        shelfCoupled = true;
-        //coupledShelfID = getShelfNumber();
-    }
-
-    public boolean onOrderMission() {
-        return onOrderMission;
-    }
-
-    public boolean onStockMission() {
-        return onStockMission;
-    }
-    // control methods:
-    // if idle, Robots will need to request new commands (from ?) (if applicable)
-    // at each new tick (from Master?).
-    // if not idle, Robots will need to request a path to travel (from Floor?)
+	   */
+  }
+  /**
+   * 
+   * @return returns current int x coordinate of the robot
+   */
+  public int getX(){
+    return x;
+  }
+  /**
+   * 
+   * @return returns current int y coordinate of the robot
+   */
+  public int getY(){
+    return y;
+  }
+  /**
+   * 
+   * @return returns int number of tiles moved since last charge
+   */
+  public int getDistanceTraversed(){
+    return distanceTraversed;
+  }
+  /**
+   * 
+   * @return returns "true" if robot is available for assignment and
+   * "false" if the robot is engaged in a mission
+   */
+  public boolean isIdle(){
+    return isIdle;
+  }
+  /**
+   * 
+   * @return returns "true" if the robot is carrying a shelf, and "false"
+   * if the robot is unburdened
+   */
+  public boolean shelfCoupled(){
+	  return shelfCoupled;
+  }
+  /**
+   * 
+   */
+  public void coupleShelf(){
+	  shelfCoupled = true;
+	  //coupledShelfID = getShelfNumber();	  
+  }
+  /**
+   * 
+   * @return returns "true" if the robot's current assignement
+   */
+  public boolean onOrderMission() {
+	  return onOrderMission;
+  }
+  public boolean onStockMission() {
+	  return onStockMission;
+  }
+  // control methods:
+  // if idle, Robots will need to request new commands (from ?) (if applicable)
+  // at each new tick (from Master?).
+  // if not idle, Robots will need to request a path to travel (from Floor?)
 }
--- CS2820/src/Production/RobotMaster.java
+++ CS2820/src/Production/RobotMaster.java
@@ -1,36 +1,42 @@
-package Production;
-
 /**
- *
- * @author: Sam Barth
- *
- * date last modified: 11/14/2016
- *
- * The RobotMaster class acts as the go-between for the Robots and the other
- * parts of the warehouse. After each tick, the RobotMaster will check whether
- * there is an available idle Robot, and if so, send that Robot on a mission by
- * making calls to either Inventory or Orders.
- */
+*
+* @author: Sam Barth
+*
+* date last modified: 11/14/2016
+* 
+* The RobotMaster class acts as the go-between for the Robots and
+* the other parts of the warehouse.  After each tick, the RobotMaster
+* will check whether there is an available idle Robot, and if so,
+* send that Robot on a mission by making calls to either Inventory
+* or Orders. 
+*/
+
+package oct31;
+
+import java.awt.Point;
 import java.util.ArrayList;
 
 // RobotMaster will implement clock interface
-public class RobotMaster implements Clock {
+public class RobotMaster implements Clock{
 
     private ArrayList<Robot> robots;
     private final int batteryRange = 50;
 
-    //intialize RobotMaster with some starting number of Robots
-    public RobotMaster(int numRobots) {
+    /**
+     * 
+     * @param numRobots the number of robots that will be used in the simulation
+     * (RobotMaster currently only supports 1 robot)
+     */
+    public RobotMaster(int numRobots){
         this.robots = new ArrayList<Robot>(numRobots);
         //the following for loop will individually initialize each robot at a charge location
         int i = 0;
-        for (Robot r : this.robots) {
+        for (Robot r : this.robots){
             r = new Robot(0, i);
             this.robots.add(r);
             i++;
         }
     }
-
     //the instuctions for the Robot will be passed to deployIdle
     /*
     private void deployIdle(String temp){
@@ -45,38 +51,33 @@ public class RobotMaster implements Clock {
         //about how to complete its mission
         }
     }
-     */
-    public boolean robotAvailable() {
-        for (Robot r : robots) {
-            if (r.isIdle()) {
-                return true;
-            }
+    */
+    public boolean robotAvailable(){
+        for (Robot r : robots){
+            if (r.isIdle()){return true;}
         }
         return false;
     }
-
     //Precondition: robotAvailable() returns "true"
-    private Robot getFirstAvailableRobot() {
-        for (Robot r : robots) {
-            if (r.isIdle()) {
-                return r;
-            }
-        }
-        Robot placeHolder = new Robot(0, 0);
-        return placeHolder;
+    private Robot getFirstAvailableRobot(){
+    	for (Robot r : robots){
+    		if (r.isIdle()){return r;}
+    	}
+    	Robot placeHolder = new Robot(0,0);
+    	return placeHolder;
     }
-
-    public void tick(int i) {
-        //first, we'll need to tell robots currently on missions to continue those missions
-        for (Robot r : robots) {
-            if (!r.isIdle()) {
-                //tell robot to continue mission
-            }
-        }
-        //we can end early if there are no available robots
-        if (!robotAvailable()) {
-            return;
-        }
+    /**
+     * @param i The cumulative tick number of the simulation
+     */
+    public void tick(int i){
+    	//first, we'll need to tell robots currently on missions to continue those missions
+    	for (Robot r : robots) {
+    		if (!r.isIdle()){
+    			//tell robot to continue mission
+    		}
+    	}
+    	//we can end early if there are no available robots
+        if (!robotAvailable()) {return;}
         //if there are available robots, we'll need to check for Orders
         //or stock missinos from Inventory
     }
