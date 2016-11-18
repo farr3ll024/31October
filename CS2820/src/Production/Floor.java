abe88e044e82ee1f616462113694bdb408b960fb
tfinley521 <trevor-finley@uiowa.edu>
2016-11-18 15:10:30 -0600

    Scaled to 160x200, fixed everything to work with scaling (I think)

--- CS2820/src/Production/Floor.java
+++ CS2820/src/Production/Floor.java
@@ -5,79 +5,78 @@ import java.awt.Point;
 /**
  *
  * @author TrevorFinley
- * @version 0.5
+ * @version 0.8
  *
  * The Floor class is designed to construct relative x/y coordinates of type
  * Point for all static locations such as charger, shelves, docks, etc.
  * and give all other parts of the warehouse access to them.
  * 
- * CURRENT FLOOR DIMENSIONS: 16 x 16 
+ * CURRENT FLOOR DIMENSIONS: 160 x 200 
  * NOTES: there is a setFloor() method that must be called to retrieve all data from the floor plan such as belts and shelves. 
  * Remember that arrays use values of max-1, so use 0 as the base for x and y extending up to 15.
  * All the constant x/y values have been offset by 1 to fit the array format.
  * If using Point, you must have import java.awt.Point; for an import statement.
  * The points follow paint's inverse coordinate system where (0,0) is the upper left corner, (15,15) is the bottom right corner.
  * 
- * Things still to do (that I know of): change to fullscale (most of the code has been optimized for simple scale change now), 
- * path calculator?
  */
 public class Floor 
 {
 	static int debug = 0;
 	//instance variables
-	final static int FLOOR_Y = 15; //y value of overall length of floor
-	final static int FLOOR_X = 15; //x value of overall width of floor
-	final static int SHELVES = 80; //current number of shelf spaces on floor
+	final static int FLOOR_Y = 199; //y value of overall length of floor
+	final static int FLOOR_X = 159; //x value of overall width of floor
+	static int shelfmax = FLOOR_X * FLOOR_Y; //set for the maximum possible number of shelves (unrealistic but used for allocating purposes)
+	static int s = 0; //number of shelves
 	final static int SHELF_XSTART = 3; //current X value starting point of shelf spaces
-	final static int SHELF_XEND = 13;  //current X value ending point
+	final static int SHELF_XEND = (FLOOR_X-2);  //current X value ending point
 	final static int SHELF_YSTART = 2; //current Y value starting point
-	final static int SHELF_YEND = 12; //current Y value ending point
+	final static int SHELF_YEND = (FLOOR_Y-3); //current Y value ending point
 	
     final static Point charger = new Point(0, 0);
-    final static Point recdock = new Point(15, 0);
-    final static Point picker = new Point(2, 14);
-    final static Point packer = new Point(14, 14);
-    final static Point shipdock = new Point(15, 15);
-    public static Point[] shelf = new Point[SHELVES + 1];
+    final static Point recdock = new Point(FLOOR_X, 0);
+    final static Point picker = new Point(2, (FLOOR_Y - 1));
+    final static Point packer = new Point((FLOOR_X - 1), (FLOOR_Y - 1));
+    final static Point shipdock = new Point(FLOOR_X, FLOOR_Y);
+    public static Point[] shelf = new Point[shelfmax];
     public static Point[] belt = new Point[FLOOR_X - 1];
    
     //returns the y value length of the floor
-    public int getFloorHeight()
+    public static int getFloorHeight()
     {
     	return FLOOR_Y + 1;
     }
     //returns the x value width of the floor
-    public int getFloorWidth()
+    public static int getFloorWidth()
     {
     	return FLOOR_X + 1;
     }
     //returns the number of shelves
-    public int numberOfShelves()
+    public static int numberOfShelves()
     {
-    	return SHELVES;
+    	return s;
     }
     //returns the coordinates of charger in (x, y) format
-    public Point getCharger()
+    public static Point getCharger()
     {
     	return charger;
     }
     //returns coordinates of receiving dock
-    public Point getReceiving()
+    public static Point getReceiving()
     {
     	return recdock;
     }
     //returns coordinates of shipping dock
-    public Point getShipping()
+    public static Point getShipping()
     {
     	return shipdock;
     }
     //returns coordinates of picker station
-    public Point getPicker()
+    public static Point getPicker()
     {
     	return picker;
     }
     //returns coordinates of packer station
-    public Point getPacker()
+    public static Point getPacker()
     {
     	return packer;
     }
@@ -95,13 +94,13 @@ public class Floor
 
     }
     //returns belt (array of coordinates)
-    public Point[] getBelt()
+    public static Point[] getBelt()
     {
     	return belt;
     }
     
     //returns specified coordinates from belt array
-    public Point getBelt(int beltNum)
+    public static Point getBelt(int beltNum)
     {
     	return belt[beltNum];
     }
@@ -110,13 +109,13 @@ public class Floor
     public static void constructShelf() 
     {
         //initializes shelf array
-        for (int i = 0; i < shelf.length; i++) 
+        for (int i = 0; i < shelfmax; i++) 
         {
             shelf[i] = new Point();
         }
         
         //span y coordinate plane
-        for(int y = 2, s = 0, j = 1; y <= (FLOOR_Y - 3); y++, j++)
+        for(int y = 2, j = 1; y <= (FLOOR_Y - 3); y++, j++)
         {
         	//accounts for highway space with regard to y
         	if((j % 6) == 0)
@@ -137,12 +136,12 @@ public class Floor
     }
     
     //returns entire array of shelf coordinates
-    public Point[] getShelf()
+    public static Point[] getShelf()
     {
     	return shelf;
     }
     //returns coordinates for given shelf space
-    public Point getShelf(int n) 
+    public static Point getShelf(int n) 
     {
         if (n > shelf.length || n < 0) 
         {
@@ -155,8 +154,8 @@ public class Floor
     }
     
 
-    //creates an ASCII mock-up of the floor (for testing purposes)
-    public static void printMap() 
+    //creates an ASCII mock-up of the floor (for testing purposes, explicitly in small-scale testing)
+    private static void printMap() 
     {
         for(int n = 0; n<(FLOOR_X * 2);n++)
         {
@@ -222,19 +221,19 @@ public class Floor
         }
     }
     //prints out general statistics of floor (for testing purposes)
-    public static void printStats()
+    private static void printStats()
     {
         System.out.println("Current Floor Dimensions: " + (FLOOR_X + 1) + "x" + (FLOOR_Y + 1));
-        System.out.println("Number of Shelves: " + SHELVES);
-        System.out.println("Charging Station: (" + ((int)charger.getX()) + ", " + ((int)charger.getY()) + ")");
-        System.out.println("Receiving Dock: (" + ((int)recdock.getX()) + ", " + ((int)recdock.getY()) + ")");
-        System.out.println("Shipping Dock: (" + ((int)shipdock.getX()) + ", " + ((int)shipdock.getY()) + ")");
-        System.out.println("Picker Station: (" + ((int)picker.getX()) + ", " + ((int)picker.getY()) + ")");
-        System.out.println("Packer Station: (" + ((int)packer.getX()) + ", " + ((int)packer.getY()) + ")");
-        System.out.println("Belt: (" + ((int)belt[0].getX())+", "+((int)belt[0].getY())+")"+" to "
-        +"(" + ((int)belt[belt.length-1].getX())+", "+((int)belt[belt.length-1].getY())+")");
-        System.out.println("Shelf area: (" + ((int)shelf[0].getX())+", "+((int)shelf[0].getY())+")"+" to "
-        +"(" + ((int)shelf[belt.length-1].getX())+", "+((int)shelf[belt.length-1].getY())+")");        
+        System.out.println("Number of Shelves: " + numberOfShelves());
+        System.out.println("Charging Station: (" + ((int)getCharger().getX()) + ", " + ((int)getCharger().getY()) + ")");
+        System.out.println("Receiving Dock: (" + ((int)getReceiving().getX()) + ", " + ((int)getReceiving().getY()) + ")");
+        System.out.println("Shipping Dock: (" + ((int)getShipping().getX()) + ", " + ((int)getShipping().getY()) + ")");
+        System.out.println("Picker Station: (" + ((int)getPicker().getX()) + ", " + ((int)getPicker().getY()) + ")");
+        System.out.println("Packer Station: (" + ((int)getPacker().getX()) + ", " + ((int)getPacker().getY()) + ")");
+        System.out.println("Belt: (" + ((int)getBelt()[0].getX())+", "+((int)getBelt()[0].getY())+")"+" to "
+        +"(" + ((int)getBelt()[belt.length-1].getX())+", "+((int)getBelt()[belt.length-1].getY())+")");
+        System.out.println("Shelf area: (" + ((int)getShelf()[0].getX())+", "+((int)getShelf()[0].getY())+")"+" to "
+        +"(" + ((int)getShelf()[s - 1].getX())+", "+((int)getShelf()[s - 1].getY())+")");        
        
     }
     //sets up floor plan
