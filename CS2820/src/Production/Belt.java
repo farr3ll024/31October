e8ab66da8b0410e20a32355829093461dfd83ca8
Michael Gibler <Michael Gibler@Prometheus>
2016-12-06 17:54:22 -0600

    Added code to tell Order when bin is ready to be filled again & Clean-up

--- CS2820/src/Production/Belt.java
+++ CS2820/src/Production/Belt.java
@@ -7,15 +7,15 @@ import java.util.Map;
 /**
  * @author: Michael Gibler
  *
- * Date last modified: 11/14/2016
+ * Date last modified: 12/6/2016
  *
- * The belt class creates an array of size 14, which is the java representation
- * of a conveyor belt. Item's can then be placed on the belt through the addItem
- * method. The belt is then moved by the beltButton method. Each time the
- * "button" is pressed, all items are moved right by one space, i.e. one spot
- * further in the array. There are a couple boolean variables to tell methods
- * when the belt is moving vs when it isn't. This helps to prevent items from
- * being lost or deleted during the running of the code.
+ * The belt class creates a hashmap of variable size, which is the java representation
+ * of a conveyor belt. The size is given to the belt class through the master class.
+ * Item's can then be placed on the belt through the addItem method. 
+ * The belt moves one space every tick, represented in the code by assigned each hashmap value to the next key (i.e 
+ * the value at key = 0 is reassigned with key = 1. This is done from the end of the belt to the front in order to prevent
+ * overwriting anything. Once the reassignment reaches the first spot in the hashmap, the program checks the Order class
+ * to see if a new bin is ready to be added to the belt.
  */
 public class Belt implements Clock, Document {
 
@@ -24,7 +24,7 @@ public class Belt implements Clock, Document {
     /*Counter variables for constructing and moving respectively*/
     static int i, j;
     /*This is the x variable given by the masterclass decremented by two since there are two open*/
- /*floor squares in the belt's row*/
+    /*floor squares in the belt's row*/
     static int beltLength;
 
 
@@ -32,12 +32,12 @@ public class Belt implements Clock, Document {
     public static void beltConstructor(int x) {
         beltLength = x - 2;
         /*This will build the belt with a Hashmap. Each key corresponds*/
- /*to a position on the belt, with an arraylist value representing the bin in that location*/
+        /*to a position on the belt, with an arraylist value representing the bin in that location*/
         for (i = 0; i < beltLength; i++) {
             belt.put(i, null);
         }
     }
-
+    /* Ticker method that moves the bins on the belt along each tick, and then checks for new bins*/
     @Override
     public void tick(int iteration) {
         /* This variable j is decreasing from the last belt Key to 1 in order to move the ArrayLists by one*/
@@ -49,10 +49,10 @@ public class Belt implements Clock, Document {
         /*Checks the Order Class to see if a bin is ready to be loaded onto belt position 0*/
         if (Orders.binFilled == true) {
             belt.put(0, Orders.orderBin);
+            Orders.binFilled = false;
         }
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
+    /* Method to document what the belt is doing and to make sure it is working correctly*/
     @Override
     public void doc() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
