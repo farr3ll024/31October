84c9713d8285963ac17e5903805fabebb97fdaad
sambarth <samuel-barth@uiowa.edu>
2016-12-06 00:12:03 -0600

    Updated package name

---
+++ CS2820/test/ProductionTestPackage/MockFloor.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ * @author Sam Barth
+ * date last modified: 11/30/2016
+ * 
+ * MockFloor is a test class available to anyone who needs to use a Floor method
+ * stub
+ */
+package ProductionTestPackage;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author barts_000
+ */
+public class MockFloor {
+    
+    public MockFloor(){
+        
+    }
+    /**
+     * getPicker stub that instantiates a Point object and returns it
+     * 
+     * @return returns a Point object simulating the location of the the picker
+     * on the Floor
+     */
+    public Point getPicker(){
+        Point picker = new Point(14, 0);
+        return picker;
+    }
+    /**
+     * getReceiving stub that instantiates a Point object and returns it
+     * 
+     * @return returns a Point object simulating the location of the the receiving
+     * dock on the Floor
+     */
+    public Point getReceiving(){
+        Point receiving = new Point(0,15);
+        return receiving;
+    }
+    
+}
