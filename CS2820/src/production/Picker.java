7c99e897d0d51d5b809c11d0f2c59138486a112c
sambarth <samuel-barth@uiowa.edu>
2016-12-07 16:52:42 -0600

    Cleaned up Picker and added methods beginNewOrder and completeOrder

--- CS2820/src/production/Picker.java
+++ CS2820/src/production/Picker.java
@@ -1,35 +1,46 @@
-///**
-// *
-// * @author Sam Barth
-// * @author Ruben Chavez date last modified: 12/7/2016
-// */
+/**
+ *
+ * @author Sam Barth
+ * @author Ruben Chavez date last modified: 12/7/2016
+ *
+ */
 package production;
-//
 
 public class Picker {
-//
-//   private String item;
-//
-
+    
+    Bin currentBin;
+    
+    /**
+     * 
+     */
     public Picker() {
-//
-//        for (string item : Order.getItems()) {
-//
     }
-//    }
-//    //@param i The String representation of the item to be picked and added to the bin
-//
-//    public void pickItem(String i)
-//        ){
-//
-//		if (Order.orderStack()) {
-//            Order.createNewOrder();
-//        }
-//
-//        this.i = item;
-//        shelf.removeItem(item);
-//        Bin.addItem(item);
-//        orders.shelfPicked = True;
-//
-//    }
+
+    /**
+     *
+     * @param i The String representation of the item to be picked and added to
+     * the bin
+     * @param s The Shelf object in which the item is contained
+     * 
+     * Postcondition: the item "item" has been added to bin and removed from
+     * the shelf
+     */
+    public void pick(String item, Shelf s) {
+        if (s.containsItem(item)) {
+            s.removeItem(item);
+            this.currentBin.addItem(item);
+        }
+    }
+    /**
+     * 
+     * @param o Order corresponding to the order desired to bin
+     */
+    public void beginNewOrder(Order o){
+        currentBin = new Bin(o);
+    }/**
+     * 
+     */
+    public void completeOrder(){
+        //give the Belt the currentBin
+    }
 }
