e477ea219abc5f17c886511a170c2da78e8a63fe
sambarth <samuel-barth@uiowa.edu>
2016-12-06 21:32:43 -0600

    Made corrections to errors found in move.Robot() after JUnit testing.  Corrected package import calls in Robot.java and RobotMaster.java

--- CS2820/src/Production/RobotMaster.java
+++ CS2820/src/Production/RobotMaster.java
@@ -1,24 +1,21 @@
 /**
-*
-* @author: Sam Barth
-*
-* date last modified: 11/30/2016
-* 
-* The RobotMaster class acts as the go-between for the Robots and
-* the other parts of the warehouse.  After each tick, the RobotMaster
-* will check whether there is an available idle Robot, and if so,
-* send that Robot on a mission by making calls to either Inventory
-* or Orders.
-*/
-
+ *
+ * @author: Sam Barth
+ *
+ * date last modified: 11/30/2016
+ *
+ * The RobotMaster class acts as the go-between for the Robots and
+ * the other parts of the warehouse.  After each tick, the RobotMaster
+ * will check whether there is an available idle Robot, and if so,
+ * send that Robot on a mission by making calls to either Inventory
+ * or Orders.
+ */
 package Production;
 
 import java.awt.Point;
 import java.util.ArrayList;
 
-import ProductionTestPackage.MockFloor;
-import ProductionTestPackage.MockOrders;
-import ProductionTestPackage.MockInventory;
+import testpackage.*;
 
 // RobotMaster will implement clock interface
 public class RobotMaster implements Clock, Document {
@@ -30,72 +27,74 @@ public class RobotMaster implements Clock, Document {
     private MockFloor f;
 
     /**
-     * 
+     *
      * @param numRobots the number of robots that will be used in the simulation
      * (RobotMaster currently only supports 1 robot)
-     * @param f is the instance of the Floor that Master will use in conjunction with
-     * this instance of RobotMaster
-     * @param o is the instance of the Orders that Master will use in conjunction with
-     * this instance of RobotMaster
-     * @param i is the instance of the Inventory that Master will use in conjunction
+     * @param f is the instance of the Floor that Master will use in conjunction
      * with this instance of RobotMaster
+     * @param o is the instance of the Orders that Master will use in
+     * conjunction with this instance of RobotMaster
+     * @param i is the instance of the Inventory that Master will use in
+     * conjunction with this instance of RobotMaster
      */
-    public RobotMaster(int numRobots, MockFloor f, MockInventory i){
+    public RobotMaster(int numRobots, MockFloor f, MockInventory i) {
         this.robots = new ArrayList<>(numRobots);
         this.lastDeployedOrder = false;
         //the following for loop will individually initialize each robot at a charge location
         int j = 0;
-        for (Robot r : this.robots){
+        for (Robot r : this.robots) {
             Point start = new Point(0, j);
             r = new Robot(start, f);
             this.robots.add(r);
             j++;
         }
     }
+
     /**
      * The precondition for calling this method is that Robot r is idle
-     * 
+     *
      * @param r Robot r is an idle Robot in the simulation
      */
-    private void deploy(Robot r){
-        if (this.lastDeployedOrder){
+    private void deploy(Robot r) {
+        if (this.lastDeployedOrder) {
             this.lastDeployedOrder = false;
             r.assignMission("Stock", VOIDLOCATION); //fill in Inventory.getShelfLocation()
-        }
-        else{
+        } else {
             r.assignMission("Order", VOIDLOCATION); //fill in Inventory.getShelfLocation()
         }
     }
-    public ArrayList getRobotLocations(){
+
+    public ArrayList getRobotLocations() {
         ArrayList<Point> robotLocations = new ArrayList<>();
-        for (Robot r : robots){
+        for (Robot r : robots) {
             Point location = new Point(r.getLocation());
             robotLocations.add(location);
         }
         return robotLocations;
     }
+
     /**
-     * Increment time by one position.  Robots deployed on missions may move
-     * a maximum of one unit per tick.
-     * 
+     * Increment time by one position. Robots deployed on missions may move a
+     * maximum of one unit per tick.
+     *
      * @param i The cumulative tick number of the simulation
-     * 
+     *
      */
     @Override
-    public void tick(int i){
+    public void tick(int i) {
         /**
-         * robots currently on missions should continue those missions, and
-         * idle robots should be deployed
-        */ 
-    	for (Robot r : robots) {
-            if (!r.isIdle()){
-                r.move();
-            }
-            else{
+         * robots currently on missions should continue those missions, and idle
+         * robots should be deployed
+         */
+        for (Robot r : robots) {
+            if (!r.isIdle()) {
+                r.move(false);
+            } else {
                 deploy(r);
             }
-    	}
+        }
     }
+
     /**
      * Document movement of Robots on the Floor
      */
